979f236896a1b977704e7a9a374547f54bdf6b83
Sergey Degtyar <degtyar2023@mail.ru>
2023-01-21 19:01:10 +0300

    master

---
+++ createdb.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+db = sqlite3.connect('data.db')
+sql = db.cursor()
+
+sql.execute("""CREATE TABLE IF NOT EXISTS tags (
+    id INTEGER PRIMARY KEY, 
+    title TEXT,
+    price BIGINT
+)""")
+
+db.commit()
+
+tag_title = input("Кличка:")
+tag_price = int(input("Стоимость клички:"))
+
+sql.execute(f"INSERT INTO tags VALUES (?,?,?)", (None, tag_title, tag_price))
+db.commit()
+
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
