01276c2295f0b8980177ce3e80e9177f8034c528
Sergey Degtyar <degtyar2023@mail.ru>
2023-01-24 14:16:30 +0300

    master

--- handlers/client.py
+++ handlers/client.py
@@ -1,16 +1,24 @@
 from aiogram import types, Dispatcher
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove, InlineKeyboardButton, InlineKeyboardMarkup
-# from instances import dp, bot
+from instances import dp, bot
 from keyboards import start_keyboard, actions_keyboard, generate_tag_keyboard
 import sqlite3, applogger
 
 logger = applogger.get_logger(__name__)
 
 async def process_start_command(message: types.Message):
-    await message.answer("Some text!", reply_markup=start_keyboard)
+    await message.answer("Стартовая клавиатура!", reply_markup=start_keyboard)
 
 async def process_start_callback(query: types.CallbackQuery):
     await query.message.edit_text('Главное меню', reply_markup=start_keyboard)
+
+async def process_unban_callback(query: types.CallbackQuery):
+    bot.get_chat_member()
+    await query.message.edit_text("Стоимость разблокировки стоит 2 рубля.", reply_markup=InlineKeyboardMarkup().add(InlineKeyboardButton(text='Оплатить', callback_data='buy')).add(InlineKeyboardButton(text='Проверить оплату', callback_data='checkpayment')))
+
+
+
+
 
 async def process_actions_callback(query: types.CallbackQuery):
     await query.message.edit_text('Доступные действия на данный момент!', reply_markup=actions_keyboard)
@@ -26,6 +34,7 @@ async def process_buytag__payment_callback(query: types.CallbackQuery):
 def register_handlers_client(dp : Dispatcher):
     dp.register_message_handler(process_start_command, commands=['start'])
     dp.register_callback_query_handler(process_start_callback, text="start")
+    dp.register_callback_query_handler(process_unban_callback, text="unban")
     dp.register_callback_query_handler(process_actions_callback, text="actions")
     dp.register_callback_query_handler(process_buytag_callback, text="buytag")
     dp.register_callback_query_handler(process_buytag__payment_callback, lambda tag: tag.data)   
